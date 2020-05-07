0d35a91532a8536e34482edd5174eaae9bd2e106
Vijay Shinde <vijay.shinde@vergehealth.com>
2020-05-08 01:20:34 +0530

    add travis file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule
